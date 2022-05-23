98bfe44e01abb3e2f216e03d21c2154e066b5cad
Bruno Marques <blmarques.dev@gmail.com>
2022-05-23 19:25:52 -0300

    Add procfile

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const routes = require("express").Router();
 const nnfRouter = require("./nnfreelas");
 const workanaRouter = require("./workanaRouter");
 
-router.get('/', (_req, res) => res.status(210).send('Hello World!'));
+routes.get('/', (_req, res) => res.status(210).send('Hello World!'));
 
 routes.use("/nnf", nnfRouter);
 
